09c319ac1055a96fe6c48bd4cd7181535de787b6
Vinicius Milan <56649132+viniciusmilan8@users.noreply.github.com>
2023-06-16 20:52:57 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,16 +4,59 @@ function appendNumber(number) {
     display.value += number;
 }
 
-// Implementação das funcionalidades do cálculo de IMC
-function calcularIMC() {
-    const altura = parseFloat(document.getElementById('altura').value);
-    const peso = parseFloat(document.getElementById('peso').value);
-    const resultado = peso / (altura * altura);
-    const resultadoIMC = document.getElementById('resultado-imc');
-    resultadoIMC.textContent = 'Seu IMC é: ' + resultado.toFixed(2);
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value += operator;
 }
 
-// Implementação das funcionalidades do formulário e geração de JSON
+function calculate() {
+    const display = document.getElementById('display');
+    const result = eval(display.value);
+    display.value = result;
+}
+
+// Funcionalidades do menu
+function changePage(pageId) {
+    const pages = document.getElementsByTagName('section');
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+
+    const menuItems = document.getElementsByClassName('menu-item');
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].classList.remove('active');
+    }
+
+    const pageToShow = document.getElementById(pageId);
+    pageToShow.style.display = 'block';
+
+    const menuItem = document.querySelector(`[onclick="changePage('${pageId}')"]`);
+    menuItem.classList.add('active');
+}
+
+// Cálculo de IMC
+function calcularIMC() {
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+
+    const imc = peso / (altura * altura);
+    let resultado = '';
+
+    if (imc < 18.5) {
+        resultado = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+        resultado = 'Peso normal';
+    } else if (imc >= 24.9 && imc < 30) {
+        resultado = 'Sobrepeso';
+    } else {
+        resultado = 'Obesidade';
+    }
+
+    const resultadoIMC = document.getElementById('resultado-imc');
+    resultadoIMC.innerText = `Seu IMC é ${imc.toFixed(2)} (${resultado})`;
+}
+
+// Geração de JSON a partir do formulário
 function gerarJSON() {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
@@ -28,22 +71,21 @@ function gerarJSON() {
     };
 
     const jsonOutput = document.getElementById('json-output');
-    jsonOutput.textContent = JSON.stringify(data, null, 2);
+    jsonOutput.innerText = JSON.stringify(data, null, 2);
 }
 
-function changePage(pageId) {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
-
-    let selectedSection;
-
-    if (pageId === 'home') {
-        selectedSection = document.getElementById('calculadora');
-    } else {
-        selectedSection = document.getElementById(pageId);
-    }
-
-    selectedSection.style.display = 'block';
+// Função para apagar valores na calculadora
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
 }
+
+// Limpar dados no gerador de JSon
+function limparDados() {
+    document.getElementById('nome').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('telefone').value = '';
+    document.getElementById('senha').value = '';
+    document.getElementById('json-output').textContent = '';
+}
+
